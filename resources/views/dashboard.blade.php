4160a8eed9b69266978b4d57a2b7cf971b41c4c6
Cadu <cadub6@gmail.com>
2025-08-05 12:41:20 -0300

    correcao titulo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,7 @@
 @extends('layout')
+@section('title', 'Dashboard da Frota')
 @props(['caminhoes'])
 @section('content')
-
-    <h1 class="text-3xl font-bold mb-6">Dashboard da Frota</h1>
     
     <!-- Cria um grid para organizar os cards -->
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
