1c19386bb8708e83cc4c30821a9732da884fdffa
Cadu <cadub6@gmail.com>
2025-08-08 09:06:31 -0300

    add cities e states

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             MarcaSeeder::class,
             ModeloSeeder::class,
+            StatesSeeder::class,
+            CitiesSeeder::class,
         ]);
         
     }
