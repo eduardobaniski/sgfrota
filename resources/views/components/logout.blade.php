42026460befb3d0e7c18566c46566d2f63a4c588
Cadu <cadub6@gmail.com>
2025-08-12 08:58:57 -0300

    edit route logout

--- resources/views/components/logout.blade.php
+++ resources/views/components/logout.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <form method="POST" action="/logout">
+    <form method="POST" action="{{ route('logout') }}">
         @csrf
         <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 m-2 rounded transition duration-300">
             Sair
