872d2b12e0408cb754a78d8e06497634bb652a52
Cadu <cadub6@gmail.com>
2025-08-22 12:43:07 -0300

    correcao da tela inicial

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,13 @@
 @vite('resources/css/app.css')
-
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="{{ asset('logo.png') }}">
+    <title>SGFrota</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
 <div class="flex items-center justify-center min-h-screen bg-gray-100">
     <form action="/login" method="POST" class="bg-white p-8 rounded shadow-md w-full max-w-sm">
